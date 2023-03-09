d4ec115a18739fd172e60c833f1778ec1b851bd1
el <el@el.com>
2023-03-09 13:26:45 +0300

    create

---
+++ config.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token = ""  
+  zone = "ru-central1-b"
+  cloud_id = "b1g8au9em58afkdtkahm"
+  folder_id = "b1go28jbjr6v23i268qj"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "test-vm-1"
+
+  resources {
+    cores = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+        image_id = "fd8snjpoq85qqv0mk9gi"
+        type = ssd
+        size = 15
+    }
+  }
+
+  network_interface {
+    subnet_id = "e2l0aklkamuvt9s69baf"
+    nat = true
+  }
+
+  metadata = {
+    user_data = "${file("~/terraform1/meta.txt")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
