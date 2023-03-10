a4a9cc7ad73b4234979387ceb6f2c8ff6f5eb4d4
el <el@el.com>
2023-03-10 16:19:44 +0300

    test

--- hw/config_hw.tf
+++ hw/config_hw.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  token = YC_TOKEN
+  service_account_key_file = "${file("~/authotized_key.json")}"
   zone = "ru-central1-b"
   cloud_id = "b1g8au9em58afkdtkahm"
   folder_id = "b1go28jbjr6v23i268qj"
