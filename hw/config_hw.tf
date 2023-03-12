449e3c8b5ce1395602f3b1c6e200fbc2248fd213
el <el@el.com>
2023-03-12 11:48:38 +0300

    test

--- hw/config_hw.tf
+++ hw/config_hw.tf
@@ -14,7 +14,18 @@ provider "yandex" {
   folder_id = "b1go28jbjr6v23i268qj"
 }
 
-resource "yandex_compute_instance" "vm-1" {
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description = "static access key for object storage"
+}
+
+resource "yandex_storage_bucket" "bckt-1" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket = "demo-bucket" 
+}
+
+/*resource "yandex_compute_instance" "vm-1" {
   name = "demo-build"
   hostname = "demo-build"
   platform_id = "standard-v3"
