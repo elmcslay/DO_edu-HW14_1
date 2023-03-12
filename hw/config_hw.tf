22edd6032b779bce8e97e41ddb964b5e3d31281b
el <el@el.com>
2023-03-12 12:17:59 +0300

    test

--- hw/config_hw.tf
+++ hw/config_hw.tf
@@ -18,6 +18,11 @@ resource "yandex_iam_service_account" "sa" {
   name = "service1"
 }
 
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description = "static access key for object storage"
+}
+
 resource "yandex_storage_bucket" "bckt-1" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
