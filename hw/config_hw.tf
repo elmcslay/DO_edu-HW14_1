bc3c390a05ca1623d61f4c091148fdf386ccded9
el <el@el.com>
2023-03-12 11:50:03 +0300

    fix

--- hw/config_hw.tf
+++ hw/config_hw.tf
@@ -14,11 +14,6 @@ provider "yandex" {
   folder_id = "b1go28jbjr6v23i268qj"
 }
 
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = yandex_iam_service_account.sa.id
-  description = "static access key for object storage"
-}
-
 resource "yandex_storage_bucket" "bckt-1" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
